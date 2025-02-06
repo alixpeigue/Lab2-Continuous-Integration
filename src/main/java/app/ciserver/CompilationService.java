0c846cb5d087dca3eeae6a2a92f1af0f44cbcfc3
Adam Rasmussen <adamfriden00@gmail.com>
2025-02-06 14:49:14 +0100

    Fix: Fix syntax errors.
    The previous version had some syntax errors.

--- src/main/java/app/ciserver/CompilationService.java
+++ src/main/java/app/ciserver/CompilationService.java
@@ -1,14 +1,15 @@
 package app.ciserver;
 
 public class CompilationService {
-    private ProcessBuilder processBuilder; // For running processes. 
-    CompilationService(){ // Needs the IOhandler 
-        // TODO
-    }
-    
-    public string compile(){ // Not sure about the input parameters here yet.
-        // TODO
-    }
+	private ProcessBuilder processBuilder; // For running processes.
 
+	public CompilationService() { // Needs the IOhandler
+		// TODO
+	}
+
+	public String compile() { // Not sure about the input parameters here yet.
+		// TODO
+		return " ";
+	}
 
 }

013ed877894c64bd0803aaf80011b5673ab0bce8
Adam Rasmussen <adamfriden00@gmail.com>
2025-02-06 14:03:05 +0100

    Feat: #2 Added skeleton compilation service
    Added a basic code skeleton for the compilation service.

---
+++ src/main/java/app/ciserver/CompilationService.java
@@ -0,0 +1,14 @@
+package app.ciserver;
+
+public class CompilationService {
+    private ProcessBuilder processBuilder; // For running processes. 
+    CompilationService(){ // Needs the IOhandler 
+        // TODO
+    }
+    
+    public string compile(){ // Not sure about the input parameters here yet.
+        // TODO
+    }
+
+
+}

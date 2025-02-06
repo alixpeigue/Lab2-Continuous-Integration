53ac78c36e850a022dee1c45683e37d5308e3fe0
Leo <leo.lundberg99@gmail.com>
2025-02-06 14:09:55 +0100

    Refactor: #12 add notification skeleton

---
+++ src/main/java/app/ciserver/NotificationService.java
@@ -0,0 +1,14 @@
+package app.ciserver;
+import java.net.http.HttpRequest; 
+
+public class NotificationService {
+
+    public void notifySuccess(String message) {}
+
+    public void notifyFailure(String message) {}
+
+    public void notifyPending() {}
+
+    public void notifyServerError(String message) {}
+
+}

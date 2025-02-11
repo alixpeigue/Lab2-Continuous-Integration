eb692ad9e3e56501d069a143060d5882119a6cb3
Adam <adamfriden00@gmail.com>
2025-02-11 22:18:17 +0100

    Refactor: Remove redundant parameters for compile().
    Removed gradlewDir parameter as the gradlew path will be projectDir +
    "/gradlew". Added missing javadoc.

--- src/main/java/app/ciserver/CompilationService.java
+++ src/main/java/app/ciserver/CompilationService.java
@@ -11,20 +11,16 @@ public class CompilationService {
 	/**
 	 * Builds gradle project at <b>projectDir</b>
 	 * <p>
-	 * using the gradlew executable at <b>gradleDir</b>.
+	 * using the gradlew executable at "<b>projectDir</b> + /gradlew".
 	 * <p>
-	 * If any of the parameters are null the method assumes the gradlew executable
-	 * is in the working directory.
 	 * 
-	 * @param gradleDir
-	 *            - Full path to gradlew executable.
 	 * @param projectDir
 	 *            - Full path to gradle project.
 	 * @return - The exit code and content from standard output and standard error.
 	 */
-	public CommandResult compile(String gradleDir, String projectDir) { //
-		CommandResult result = commandService.runCommand(new String[]{gradleDir, "-p", projectDir, "build"});
-
+	public CommandResult compile(String projectDir) {
+		CommandResult result = commandService
+				.runCommand(new String[]{projectDir + "/gradlew", "-p", projectDir, "build"});
 		return result;
 	}
 
--- src/test/java/app/ciserver/CompilationServiceTest.java
+++ src/test/java/app/ciserver/CompilationServiceTest.java
@@ -14,38 +14,46 @@ public class CompilationServiceTest {
 	static CommandService commandService;
 
 	@BeforeEach
-	void addService() {
+	void addServices() {
 		commandService = mock(CommandService.class);
 		compilationService = new CompilationService(commandService);
 	}
 	/**
 	 * <p>
-	 * Mock test to see that the command given to the compile method
+	 * Mock test to see that the invalid command given to the compile method
 	 * </p>
 	 * <p>
-	 * Is the same one called by commandService.runCommand()
+	 * is the same one passed to commandService.runCommand().
 	 * </p>
 	 */
 	@Test
 	void invalidCompilation() {
-		String[] command = {"invalid/path", "-p", "path/to/project", "build"};
+		String[] command = {"invalid/path/to/project/gradlew", "-p", "invalid/path/to/project", "build"};
 
 		when(commandService.runCommand(any())).thenReturn(new CommandResult(1, "error"));
 
-		CommandResult result = compilationService.compile("invalid/path", "path/to/project");
+		CommandResult result = compilationService.compile("invalid/path/to/project");
 
 		verify(commandService).runCommand(command);
 
 		assertEquals(1, result.exitCode());
 	}
 
+	/**
+	 * <p>
+	 * Mock test that ensure proper communication with the commandService
+	 * </p>
+	 * <p>
+	 * when given a valid command.
+	 * </p>
+	 */
 	@Test
 	void validCompilation() {
-		String[] command = {"path/to/gradlew", "-p", "path/to/project", "build"};
+		String[] command = {"path/to/project/gradlew", "-p", "path/to/project", "build"};
 
 		when(commandService.runCommand(any())).thenReturn(new CommandResult(0, "success"));
 
-		CommandResult result = compilationService.compile("path/to/gradlew", "path/to/project");
+		CommandResult result = compilationService.compile("path/to/project");
 
 		verify(commandService).runCommand(command);
 

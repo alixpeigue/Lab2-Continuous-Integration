17ef15e8c660e4967cde5d0589cbe67b6bd5b8ca
Leo Lundberg <46373710+SetiQyu@users.noreply.github.com>
2025-02-11 19:02:00 +0100

    Feat: #14 Notification service functionality (#39)
    * Feat: #14 Notification service functionality
    
    Implements the Notification Service and the tests
    
    ---------
    
    Co-authored-by: Alix Peigue <irobot.moi@gmail.com>

--- src/main/java/app/ciserver/NotificationService.java
+++ src/main/java/app/ciserver/NotificationService.java
@@ -1,17 +1,67 @@
 package app.ciserver;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class NotificationService {
 
-	public void notifyFailure(String message) {
+	// TODO replace and integrate with webhook
+	private static final Logger LOGGER = LoggerFactory.getLogger(CommandService.class);
+
+	HttpClient.Builder clientBuilder() {
+
+		return HttpClient.newBuilder();
+	}
+	String getGithubToken() {
+		return System.getenv("GITHUB_API_TOKEN");
+	}
+	void handleResponse(HttpResponse<String> responseMessage) {
+		int responseCode = responseMessage.statusCode();
+		if (responseCode != 200) {
+			LOGGER.error("Error while waiting for response '{}' : {}", String.join(" ", "status code"),
+					String.valueOf(responseCode));
+		}
 	}
 
-	public void notifyPending() {
+	public void notifyError(String description, HookEventModel pathParams) {
+		requestWrapper(new CommitStatusModel("error", description, "continuous-integration/ciserver"), pathParams);
 	}
 
-	public void notifyServerError(String message) {
+	public void notifyFailure(String description, HookEventModel pathParams) {
+		requestWrapper(new CommitStatusModel("failure", description, "continuous-integration/ciserver"), pathParams);
 	}
 
-	public void notifySuccess(String message) {
+	public void notifyPending(String description, HookEventModel pathParams) {
+		requestWrapper(new CommitStatusModel("pending", description, "continuous-integration/ciserver"), pathParams);
 	}
+	public void notifySuccess(String description, HookEventModel pathParams) {
 
+		requestWrapper(new CommitStatusModel("success", description, "continuous-integration/ciserver"), pathParams);
+	}
+	HttpRequest.Builder requestBuilder() {
+
+		return HttpRequest.newBuilder();
+	}
+	void requestWrapper(CommitStatusModel bodyParams, HookEventModel pathParams) {
+		try {
+			String json = new ObjectMapper().writeValueAsString(bodyParams);
+			HttpClient client = clientBuilder().build();
+			HttpRequest request = requestBuilder()
+					.uri(URI.create(pathParams.repository().fullName() + "/statuses/" + pathParams.after()))
+					.header("Accept", "application/vnd.github+json")
+					.header("Authorization", "Bearer " + getGithubToken()).header("X-GitHub-Api-Version", "2022-11-18")
+					.timeout(Duration.ofMinutes(2)).POST(BodyPublishers.ofString(json)).build();
+			handleResponse(client.send(request, BodyHandlers.ofString()));
+		} catch (IOException | InterruptedException e) {
+			LOGGER.error("Error while attempting to send request '{}' : {}", String.join(" ", "requestWrapper fail"),
+					e.getMessage());
+		}
+	}
 }
---
+++ src/test/java/app/ciserver/NotificationServiceTest.java
@@ -0,0 +1,124 @@
+package app.ciserver;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+class NotificationServiceTest {
+
+	static String uriStub;
+	static HookEventModel pathParams;
+
+	@BeforeAll
+	static void testSetup() {
+		uriStub = "https://www.example.com";
+		pathParams = new HookEventModel("sha23", "ref", new PusherModel("silly@mail", "name", "username"),
+				new RepositoryModel("cloneUrl", "FullName"));
+	}
+
+	@Mock
+	private HttpClient mockedClient;
+
+	@Mock
+	private HttpClient.Builder mockedClientBuilder;
+
+	@Mock
+	private HttpRequest mockedRequest;
+
+	@Mock
+	private HttpRequest.Builder mockedRequestBuilder;
+
+	@Mock
+	private HttpResponse<String> mockedResponse;
+
+	@Spy
+	private NotificationService spyNotificationService;
+
+	@BeforeEach
+	void initMocks() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testNotifyError() {
+
+		CommitStatusModel bodyParams = new CommitStatusModel("error", "silly", "continuous-integration/ciserver");
+		doNothing().when(spyNotificationService).requestWrapper(any(CommitStatusModel.class),
+				any(HookEventModel.class));
+		spyNotificationService.notifyError("silly", pathParams);
+		verify(spyNotificationService).requestWrapper(bodyParams, pathParams);
+	}
+	@Test
+	void testNotifyFailure() {
+
+		CommitStatusModel bodyParams = new CommitStatusModel("failure", "silly", "continuous-integration/ciserver");
+		doNothing().when(spyNotificationService).requestWrapper(any(CommitStatusModel.class),
+				any(HookEventModel.class));
+		spyNotificationService.notifyFailure("silly", pathParams);
+		verify(spyNotificationService).requestWrapper(bodyParams, pathParams);
+	}
+
+	@Test
+	void testNotifyPending() {
+
+		CommitStatusModel bodyParams = new CommitStatusModel("pending", "silly", "continuous-integration/ciserver");
+		doNothing().when(spyNotificationService).requestWrapper(any(CommitStatusModel.class),
+				any(HookEventModel.class));
+		spyNotificationService.notifyPending("silly", pathParams);
+		verify(spyNotificationService).requestWrapper(bodyParams, pathParams);
+
+	}
+
+	@Test
+	void testNotifySuccess() {
+
+		CommitStatusModel bodyParams = new CommitStatusModel("success", "silly", "continuous-integration/ciserver");
+		doNothing().when(spyNotificationService).requestWrapper(any(CommitStatusModel.class),
+				any(HookEventModel.class));
+		spyNotificationService.notifySuccess("silly", pathParams);
+		verify(spyNotificationService).requestWrapper(bodyParams, pathParams);
+	}
+
+	@Test
+	void testRequestWrapper() throws IOException, InterruptedException {
+
+		when(mockedRequestBuilder.uri(any(URI.class))).thenReturn(mockedRequestBuilder);
+		when(mockedRequestBuilder.header(anyString(), anyString())).thenReturn(mockedRequestBuilder);
+		when(mockedRequestBuilder.timeout(any(Duration.class))).thenReturn(mockedRequestBuilder);
+		when(mockedRequestBuilder.POST(any(HttpRequest.BodyPublisher.class))).thenReturn(mockedRequestBuilder);
+		when(mockedRequestBuilder.build()).thenReturn(mockedRequest);
+
+		when(mockedClientBuilder.build()).thenReturn(mockedClient);
+
+		when(mockedClient.send(any(HttpRequest.class), any(HttpResponse.BodyHandler.class))).thenReturn(mockedResponse);
+
+		CommitStatusModel bodyParams = new CommitStatusModel("pending", "silly", "continuous-integration/ciserver");
+		doReturn(mockedClientBuilder).when(spyNotificationService).clientBuilder();
+		doReturn(mockedRequestBuilder).when(spyNotificationService).requestBuilder();
+		doNothing().when(spyNotificationService).handleResponse(any());
+		doReturn("API TOKEN EXAMPLE").when(spyNotificationService).getGithubToken();
+
+		spyNotificationService.requestWrapper(bodyParams, pathParams);
+
+		verify(mockedRequestBuilder).uri(URI.create("FullName/statuses/sha23"));
+		verify(mockedRequestBuilder).header("Accept", "application/vnd.github+json");
+		verify(mockedRequestBuilder).header("Authorization", "Bearer " + spyNotificationService.getGithubToken());
+		verify(mockedRequestBuilder).header("X-GitHub-Api-Version", "2022-11-18");
+		verify(mockedRequestBuilder).timeout(Duration.ofMinutes(2));
+		verify(mockedRequestBuilder).POST(any(HttpRequest.BodyPublisher.class));
+		verify(mockedRequestBuilder).build();
+
+		verify(mockedClient).send(mockedRequest, HttpResponse.BodyHandlers.ofString());
+		verify(mockedClientBuilder).build();
+
+		verify(spyNotificationService).handleResponse(mockedResponse);
+	}
+}

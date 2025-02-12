3c93389ff1da0a4ac7fd88598193e19d39810b86
Alix Peigue <52763123+alixpeigue@users.noreply.github.com>
2025-02-12 13:17:58 +0100

    Feat: #45 add the controller TestRunController for displaying builds (#56)
    * Feat: #45 add the controller TestRunController for displaying builds
    
    * Fix: fix formatting

---
+++ src/main/java/app/ciserver/TestRunController.java
@@ -0,0 +1,29 @@
+package app.ciserver;
+
+import io.javalin.http.Context;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class TestRunController {
+
+	TestRunPersistenceService testRunPersistenceService;
+
+	public TestRunController(TestRunPersistenceService testRunPersistenceService) {
+		this.testRunPersistenceService = testRunPersistenceService;
+	}
+
+	public void testRunDetailHandler(Context ctx) {
+		Optional<TestRunModel> testRun = testRunPersistenceService.load(ctx.pathParam("commitSHA"));
+		if (testRun.isEmpty()) {
+			ctx.status(404);
+		} else {
+			ctx.render("detail.html", Map.of("build", testRun.get()));
+		}
+	}
+
+	public void testRunListHandler(Context ctx) {
+		List<TestRunModel> testRuns = testRunPersistenceService.loadAll();
+		ctx.render("list.html", Map.of("builds", testRuns));
+	}
+}

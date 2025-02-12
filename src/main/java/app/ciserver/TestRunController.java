8206e5da4ae3c484d7bcedb21ed19d4771f2e6df
Alix Peigue <irobot.moi@gmail.com>
2025-02-12 13:08:37 +0100

    Fix: fix formatting

--- src/main/java/app/ciserver/TestRunController.java
+++ src/main/java/app/ciserver/TestRunController.java
@@ -1,30 +1,29 @@
 package app.ciserver;
 
 import io.javalin.http.Context;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class TestRunController {
 
-    TestRunPersistenceService testRunPersistenceService;
+	TestRunPersistenceService testRunPersistenceService;
 
-    public TestRunController(TestRunPersistenceService testRunPersistenceService) {
-        this.testRunPersistenceService = testRunPersistenceService;
-    }
+	public TestRunController(TestRunPersistenceService testRunPersistenceService) {
+		this.testRunPersistenceService = testRunPersistenceService;
+	}
 
-    public void testRunListHandler(Context ctx) {
-        List<TestRunModel> testRuns = testRunPersistenceService.loadAll();
-        ctx.render("list.html", Map.of("builds", testRuns));
-    }
+	public void testRunDetailHandler(Context ctx) {
+		Optional<TestRunModel> testRun = testRunPersistenceService.load(ctx.pathParam("commitSHA"));
+		if (testRun.isEmpty()) {
+			ctx.status(404);
+		} else {
+			ctx.render("detail.html", Map.of("build", testRun.get()));
+		}
+	}
 
-    public void testRunDetailHandler(Context ctx) {
-        Optional<TestRunModel> testRun = testRunPersistenceService.load(ctx.pathParam("commitSHA"));
-        if(testRun.isEmpty()) {
-            ctx.status(404);
-        } else {
-            ctx.render("detail.html", Map.of("build", testRun.get()));
-        }
-    }
+	public void testRunListHandler(Context ctx) {
+		List<TestRunModel> testRuns = testRunPersistenceService.loadAll();
+		ctx.render("list.html", Map.of("builds", testRuns));
+	}
 }
--- src/test/java/app/ciserver/TestRunControllerTest.java
+++ src/test/java/app/ciserver/TestRunControllerTest.java
@@ -1,71 +1,69 @@
 package app.ciserver;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import io.javalin.http.Context;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 class TestRunControllerTest {
 
-    @Mock
-    TestRunPersistenceService testRunPersistenceService;
+	@Mock
+	TestRunPersistenceService testRunPersistenceService;
 
-    @Mock
-    Context context;
+	@Mock
+	Context context;
 
-    @InjectMocks
-    TestRunController testRunController;
+	@InjectMocks
+	TestRunController testRunController;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-    @Test
-    void testListHandler() {
-        List<TestRunModel> testRuns = List.of(
-                new TestRunModel(null, "success", "COMMITHASH1", "main", "pusher", "Build success"),
-                new TestRunModel(null, "failure", "COMMITHASH2", "main", "pusher", "Build failure")
-        );
-        when(testRunPersistenceService.loadAll()).thenReturn(testRuns);
-        testRunController.testRunListHandler(context);
-        verify(context).render("list.html", Map.of("builds", testRuns));
-    }
+	@Test
+	void testDetailHandlerDoesNotExist() {
+		String hash = "COMMITHASH";
+		when(testRunPersistenceService.load(anyString())).thenReturn(Optional.empty());
+		when(context.pathParam(anyString())).thenReturn(hash);
 
-    @Test
-    void testDetailHandlerExist() {
-        String hash = "COMMITHASH";
-        TestRunModel testRun = new TestRunModel(null, "success", hash, "main", "pusher", "Build success");
-        when(testRunPersistenceService.load(anyString())).thenReturn(Optional.of(testRun));
-        when(context.pathParam(anyString())).thenReturn(hash);
+		testRunController.testRunDetailHandler(context);
 
-        testRunController.testRunDetailHandler(context);
+		verify(context).pathParam("commitSHA");
+		verify(context).status(404);
+	}
 
-        verify(context).pathParam("commitSHA");
-        verify(context).render("detail.html", Map.of("build", testRun));
-    }
+	@Test
+	void testDetailHandlerExist() {
+		String hash = "COMMITHASH";
+		TestRunModel testRun = new TestRunModel(null, "success", hash, "main", "pusher", "Build success");
+		when(testRunPersistenceService.load(anyString())).thenReturn(Optional.of(testRun));
+		when(context.pathParam(anyString())).thenReturn(hash);
 
-    @Test
-    void testDetailHandlerDoesNotExist() {
-        String hash = "COMMITHASH";
-        when(testRunPersistenceService.load(anyString())).thenReturn(Optional.empty());
-        when(context.pathParam(anyString())).thenReturn(hash);
+		testRunController.testRunDetailHandler(context);
 
-        testRunController.testRunDetailHandler(context);
+		verify(context).pathParam("commitSHA");
+		verify(context).render("detail.html", Map.of("build", testRun));
+	}
 
-        verify(context).pathParam("commitSHA");
-        verify(context).status(404);
-    }
+	@Test
+	void testListHandler() {
+		List<TestRunModel> testRuns = List.of(
+				new TestRunModel(null, "success", "COMMITHASH1", "main", "pusher", "Build success"),
+				new TestRunModel(null, "failure", "COMMITHASH2", "main", "pusher", "Build failure"));
+		when(testRunPersistenceService.loadAll()).thenReturn(testRuns);
+		testRunController.testRunListHandler(context);
+		verify(context).render("list.html", Map.of("builds", testRuns));
+	}
 
 }
